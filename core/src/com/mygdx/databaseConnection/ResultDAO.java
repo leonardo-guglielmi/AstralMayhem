2628b9b669ea224f51bf7c77032a52d2061ceab1
lorenzo-lavacchini <lorenzo.lavacchini@gmail.com>
2024-03-09 15:47:59 +0100

    Some changes to databaseConnection package

--- core/src/com/mygdx/databaseConnection/ResultDAO.java
+++ core/src/com/mygdx/databaseConnection/ResultDAO.java
@@ -1,68 +1,14 @@
 package com.mygdx.databaseConnection;
 
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 
-public class ResultDAO {
-    public Result get() throws SQLException {
-        Connection con = Database.getConnection();
-        Result result = null;
-        String sql = "SELECT *\n" +
-                "FROM result\n" +
-                "WHERE points = (SELECT MAX(points) FROM Result);\n";
-        PreparedStatement ps = con.prepareStatement(sql);
-        ResultSet rs = ps.executeQuery();
-        if(rs.next()){
-            String playerName = rs.getString("playerName");
-            int points = rs.getInt("points");
-            int time = rs.getInt("time");
+public interface ResultDAO {
 
-            result = new Result(playerName, points, time);
-        }
-        return result;
-    }
+    Result get() throws SQLException;
 
-    public List<Result> getAll() throws SQLException {
+    List<Result> getAll() throws SQLException;
 
-        Connection con = Database.getConnection();
-        String sql = "SELECT * FROM employees";
+    int insert(Result result) throws SQLException;
 
-        List<Result> employees = new ArrayList<>();
-
-        Statement stmt = con.createStatement();
-
-        ResultSet rs = stmt.executeQuery(sql);
-
-        while(rs.next()) {
-            String playerName = rs.getString("playerName");
-            int points = rs.getInt("points");
-            int time = rs.getInt("time");
-
-            Result result = new Result(playerName, points, time);
-
-            employees.add(result);
-        }
-
-        return employees;
-    }
-
-    public int insert(Result result) throws SQLException {
-        Connection con = Database.getConnection();
-
-        String sql = "INSERT INTO Result (playerName, points, time) VALUES (?, ?, ?)";
-
-        PreparedStatement ps = con.prepareStatement(sql);
-
-        ps.setString(1, result.getPlayerName());
-        ps.setInt(2, result.getPoints());
-        ps.setInt(3, result.getTime());
-
-        int res = ps.executeUpdate();
-
-        Database.closePreparedStatement(ps);
-        Database.closeConnection(con);
-
-        return res;
-    }
 }
---
+++ core/src/com/mygdx/databaseConnection/ResultDAOConcrete.java
@@ -0,0 +1,69 @@
+package com.mygdx.databaseConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultDAOConcrete implements ResultDAO{
+
+    public Result get() throws SQLException {
+        Connection con = Database.getConnection();
+        Result result = null;
+        String sql = "SELECT *\n" +
+                "FROM result\n" +
+                "WHERE points = (SELECT MAX(points) FROM Result);\n";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()){
+            String playerName = rs.getString("playerName");
+            int points = rs.getInt("points");
+            int time = rs.getInt("time");
+
+            result = new Result(playerName, points, time);
+        }
+        return result;
+    }
+
+    public List<Result> getAll() throws SQLException {
+
+        Connection con = Database.getConnection();
+        String sql = "SELECT * FROM result";
+
+        List<Result> employees = new ArrayList<>();
+
+        Statement stmt = con.createStatement();
+
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while(rs.next()) {
+            String playerName = rs.getString("playerName");
+            int points = rs.getInt("points");
+            int time = rs.getInt("time");
+
+            Result result = new Result(playerName, points, time);
+
+            employees.add(result);
+        }
+
+        return employees;
+    }
+
+    public int insert(Result result) throws SQLException {
+        Connection con = Database.getConnection();
+
+        String sql = "INSERT INTO Result (playerName, points, time) VALUES (?, ?, ?)";
+
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ps.setString(1, result.getPlayerName());
+        ps.setInt(2, result.getPoints());
+        ps.setInt(3, result.getTime());
+
+        int res = ps.executeUpdate();
+
+        Database.closePreparedStatement(ps);
+        Database.closeConnection(con);
+
+        return res;
+    }
+}
