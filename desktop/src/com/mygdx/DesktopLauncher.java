4bbca114914a33555e37b0e7afec37c48aa3c024
guglie <guglie.leonardo@gmail.com>
2024-03-27 19:03:18 +0100

    updated package diagram

--- desktop/src/com/mygdx/DesktopLauncher.java
+++ desktop/src/com/mygdx/DesktopLauncher.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.utils.Commons;
 
-import java.io.IOException;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
